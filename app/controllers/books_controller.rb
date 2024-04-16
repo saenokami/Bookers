183ea62c52c0a56843a9cec15c616a907c913ad4
saenokami <165577235+saenokami@users.noreply.github.com>
2024-04-16 19:46:47 +0000

    [Add]投稿フォームを作成する

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,31 @@
 class BooksController < ApplicationController
-  def new
-  end
 
-  def index
+  def new
+    # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
+    @book = Book.new
   end
+  
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to @book, notice: 'Book was successfully created.'
+    else
+      render :new
+    end
+  end 
+  
+  def index
+  end 
 
   def show
   end
 
   def edit
   end
+  
+  private
+
+def book_params
+  params.require(:book).permit(:title, :body)
 end
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -3,18 +3,7 @@ Rails.application.routes.draw do
 
   # トップ画面をルートパスに指定
   root 'homes#top'
+  post 'books' => 'books#create'
 
-  resources :books do
-    # 新規投稿用のフォームを表示するページへのルーティング
-    get 'new', on: :collection
-
-    # 投稿一覧を表示するページへのルーティング
-    get 'index', on: :collection
-
-    # 特定の投稿を表示するページへのルーティング
-    get 'show', on: :member
-
-    # 投稿を編集するページへのルーティング
-    get 'edit', on: :member
-  end
+  resources :books
 end
