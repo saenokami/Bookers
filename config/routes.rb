041322ec5f0897b43e1e0e67e4e72948bb6ad30b
saenokami <165577235+saenokami@users.noreply.github.com>
2024-04-16 17:23:17 +0000

    [Add]ルーティングの記述変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,20 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get '/top' => 'homes#top'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # 他のルート設定...
+
+  # トップ画面をルートパスに指定
+  root 'homes#top'
+
+  resources :books do
+    # 新規投稿用のフォームを表示するページへのルーティング
+    get 'new', on: :collection
+
+    # 投稿一覧を表示するページへのルーティング
+    get 'index', on: :collection
+
+    # 特定の投稿を表示するページへのルーティング
+    get 'show', on: :member
+
+    # 投稿を編集するページへのルーティング
+    get 'edit', on: :member
+  end
 end
