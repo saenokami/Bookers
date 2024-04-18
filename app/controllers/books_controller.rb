801d897865658b9a051a65830d3f50eb33647ec8
saenokami <165577235+saenokami@users.noreply.github.com>
2024-04-18 06:28:12 +0000

    [Add]フラッシュメッセージ(update時)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -29,7 +29,7 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(book.id)  
+    redirect_to book_path(book.id),notice: 'Book was successfully created.'
   end
   
   def destroy
