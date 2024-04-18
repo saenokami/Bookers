e4fa737015c65ef8d7fe9d36a83df90e8b972d50
saenokami <165577235+saenokami@users.noreply.github.com>
2024-04-18 08:18:08 +0000

    [Add]バリデーション(update時)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -29,9 +29,12 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id),notice: 'Book was successfully created.'
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
   end
   
   def destroy
